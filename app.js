b50f9be18e139c842e07eda38a5acde7cdc194f0
EBROFACTORYPROCESS <ybai@ebrofactory.com>
2026-08-27 14:37:56 +0200

    Update app.js

--- app.js
+++ app.js
@@ -266,7 +266,7 @@ function normalizeData(data) {
         }
         normalizeWorkflowData(sc.workflow);
         if (sc.workflow && sc.workflow.nodes) {
-            ensureUniqueNodeIds(sc.workflow);
+            // ensureUniqueNodeIds(sc.workflow);
             sortWorkflowNodes(sc.workflow);
         }
         if (sc.workflow && sc.workflow.nodeIdCounter !== undefined) {
